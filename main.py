636bccec3355be0e7d5c663e0a7b4bcec00e0b3d
iso4096 <45616468+iso4096@users.noreply.github.com>
2021-03-05 10:47:28 +0000

    output_2

--- main.py
+++ main.py
@@ -10,17 +10,21 @@ app_content = open("index.html", "r").read()
 
 # http://centennialbulb.org/oldbulb/ctbulb.jpg
 
+
 @app.route('/')
 def main():
-    global times, app_content
-    return app_content %(times)
+	global times, app_content
+	return app_content % (times)
+
 
 def run():
-  app.run(host="0.0.0.0", port=8000)
+	app.run(host="0.0.0.0", port=8000)
+
 
 def keep_alive():
-  server = Thread(target=run)
-  server.start()
+	server = Thread(target=run)
+	server.start()
+
 
 keep_alive()
 
@@ -28,19 +32,23 @@ times = 0
 attempts = 0
 prev_response = None
 while True:
-    now = datetime.now()
+	now = datetime.now()
 
-    if now.microsecond <= 50:
-        response = subprocess.run(['curl', 'http://centennialbulb.org/oldbulb/ctbulb.jpg', '-s', '--output', '-'], 
-            stdout=subprocess.PIPE).stdout
-        if prev_response != response and len(response) > 30000:
-            file_name = time.strftime("images/%Y%m%dT%H%M%SZ.jpg", time.localtime())
-            with open(file_name, 'wb') as image_file:
-                image_file.write(response)
-            prev_response = response
-            times += 1
-            attempts = 0
-        else:
-            attempts += 1
-        
-        print(times, attempts)
+	if now.microsecond <= 50:
+		response = subprocess.run([
+		    'curl', 'http://centennialbulb.org/oldbulb/ctbulb.jpg', '-s',
+		    '--output', '-'
+		],
+		                          stdout=subprocess.PIPE).stdout
+		if prev_response != response and len(response) > 30000:
+			file_name = time.strftime("images/%Y%m%dT%H%M%SZ.jpg",
+			                          time.localtime())
+			with open(file_name, 'wb') as image_file:
+				image_file.write(response)
+			prev_response = response
+			times += 1
+			attempts = 0
+		else:
+			attempts += 1
+
+		print(times, attempts)
